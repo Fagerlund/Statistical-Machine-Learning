88541e625e95da76daf92f4338fa20644d29303b
Henrik Fagerlund <henrikfagerlund11@gmail.com>
2022-12-01 14:29:22 +0100

    Move and push

---
+++ Project1/tfkerasTest.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import numpy
+
+
+# 1. Define the model
+
+
+
+
+
+# 2. Compile the model
+
+
+
+
+
+# 3. Fit the model
+
+
+
+
+
+# 4. Evaluate the model
+
+
+
+
+
+# 5. Make predictions
+
+
+
--- neuralnetwork.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow import keras
-# from tensorflow.kersas import layers
-import numpy
-import pandas
-
-
-print("TensorFlow version:", tf.__version__)
